0d99ca3e8559400032324abe7b104ef4c7b2a4a9
Geoff Boeing <boeing@usc.edu>
2021-09-15 10:54:33 -0700

    win scripts

---
+++ modules/windows-launch-jupyterlab.bat
@@ -0,0 +1,2 @@
+CALL "%userprofile%\miniconda3\Scripts\activate.bat"
+CALL activate ppd430 && jupyter lab
---
+++ windows-conda-activate.bat
@@ -0,0 +1,3 @@
+CALL "%userprofile%\miniconda3\Scripts\activate.bat"
+CALL activate ppd430
+CALL cmd \k
